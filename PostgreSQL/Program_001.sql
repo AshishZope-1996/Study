58e077f9a52eebd031febf792cecb4af283d1e8c
AshishZope-1996 <ashishzope1996@gmail.com>
2025-05-19 00:30:30 +0530

    Adding the file on git

---
+++ PostgreSQL/Program_001.sql
@@ -0,0 +1,5 @@
+create schema api;
+create schema master;
+create schema log;
+create schema dashbaord;
+create schema support;
---
+++ Python/001.py
@@ -0,0 +1,7 @@
+print('hello world')
+num  = 1
+num2 = 2
+num3 = 3
+
+addition = num + num2 + num3
+print(addition)
